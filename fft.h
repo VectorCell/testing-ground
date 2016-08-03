816dedd8a9194908c8ac3c37a41c1bf1468f0004
Brandon Smith <vectorcell@gmail.com>
2016-08-02 21:24:02 -0500

    fixed last commit

--- fft.h
+++ fft.h
@@ -12,7 +12,7 @@ void fillWithSines (const FI& begin, const FI& end, const FP step = 1.0)
 {
 	const std::vector<std::function<FP(FP)>> functions = {
 		[] (FP x) -> FP { return sin(x); },
-		[] (FP x) -> FP { return sin(2 * x); }
+		[] (FP x) -> FP { return sin(2 * x); },
 		[] (FP x) -> FP { return sin(4 * x); },
 		[] (FP x) -> FP { return sin(8 * x); },
 		[] (FP x) -> FP { return sin(16 * x); }
--- temp.cc
+++ temp.cc
@@ -1,76 +1,9 @@
-#include <iostream>
-#include <vector>
-
 #include <cstdio>
-#include <cstdint>
 
-#include <unistd.h>
-
-using namespace std;
-
-const size_t BLOCKSIZE = 4 * 1024;
-
-bool valid_char(char ch)
+int main(int argc, char *argv[])
 {
-	if (ch == '\0')
-		return true;
-	else if (ch == '\t')
-		return true;
-	else if (ch == '\n')
-		return true;
-	else if (ch >= ' ' && ch <= '~')
-		return true;
-	else
-		return false;
-}
-
-size_t process(const vector<uint8_t>& in, vector<uint8_t>& out, const size_t& count)
-{
-	out.resize(0);
-	for (size_t k = 0; k < count; ++k) {
-		if (valid_char(in[k])) {
-			out.push_back(in[k]);
-		}
-	}
-	return count - out.size();
-}
-
-size_t sanitize(FILE *infile, FILE *outfile)
-{
-	if (infile == NULL || outfile == NULL)
-		return 0;
-
-	vector<uint8_t> bufin(BLOCKSIZE);
-	vector<uint8_t> bufout(BLOCKSIZE);
-
-	size_t removed = 0;
-	size_t count = 0;
-	while ((count = fread(&bufin[0], 1, BLOCKSIZE, infile)) > 0) {
-		removed += process(bufin, bufout, count);
-		fwrite(&bufout[0], bufout.size(), 1, outfile);
-	}
-	return removed;
-}
-
-int main (int argc, char *argv[])
-{
-	FILE *outfile;
-	string filename;
-	if (argc >= 2) {
- 		filename = argv[1];
- 		outfile = fopen(filename.c_str(), "wb");
-	} else {
-		filename = "stdout";
-		outfile = stdout;
-	}
-
-	if (outfile == NULL) {
-		fprintf(stderr, "ERROR: unable to open file %s\n", filename.c_str());
-		return EXIT_FAILURE;
-	}
-
-	size_t removed = sanitize(stdin, outfile);
-	fprintf(stderr, "removed %zu bytes\n", removed);
-
-	return EXIT_SUCCESS;
+	int i = 0;
+	i = ++i + i++;
+	printf("%d\n", i);
+	return 0;
 }
